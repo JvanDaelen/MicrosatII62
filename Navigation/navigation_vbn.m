bb00aa3a2f82d745cae212eeb87fec56c662a03b
deepanshu6901 <deepanshupunjabi98@gmail.com>
2024-02-01 02:30:23 +0530

    Implementation of VBN for final approach and docking

--- Navigation/navigation_vbn.m
+++ Navigation/navigation_vbn.m
@@ -1,4 +1,7 @@
-function [relative_state_chaser, absolute_state_chaser] = navigation_vbn(relative_state_chaser, absolute_state_chaser, absolute_state_target, control_force)
+function [relative_state_chaser, absolute_state_chaser, P, alpha, beta, ...
+    gamma, station_keeping_points, desired_relative_state, mode] = ... 
+    navigation_vbn(relative_state_chaser, absolute_state_chaser, ...
+    absolute_state_target, control_force, P, alpha, beta, gamma, station_keeping_points)
     %% Define Initial State of Chaser S/C
 
     % Distance of chaser from target s/c after entering VBN mode
@@ -7,11 +10,6 @@ function [relative_state_chaser, absolute_state_chaser] = navigation_vbn(relativ
                             relative_state_chaser(2,:).^2 + ...
                             relative_state_chaser(3,:).^2 ...
                             );
-
-    % Attitude of the target spacecraft
-    alpha = 0;
-    beta = 0;
-    gamma = 0;
 
     %% Define the camera parameters
     focal_length = 0.2; % m, Focal length of the Camera
@@ -25,19 +23,61 @@ function [relative_state_chaser, absolute_state_chaser] = navigation_vbn(relativ
 
     d_initial = D*focal_length/range_initial; % Initial Size of the image on the Camera Sensor
 
+    Az = atand(d_initial/focal_length); % Initial Azimuth Angle
+    El = atand(d_initial/focal_length); % Initial Elevation Angle
+
     center_camera_sensor = [0; ymax/2; zmax/2]; % Position of LED-5 on the Camera Sensor for the 2 spacecrafts to be aligned
 
-    %% Define LED pattern layout on the Target S/C    
-    led1_pos = [0; d_initial; 0]; % LED-1
-    led2_pos = [0; 0; d_initial]; % LED-2
-    led3_pos = [0; -d_initial; 0]; % LED-3
-    led4_pos = [0; 0; -d_initial]; % LED-4
-    led5_pos = [-d_initial; 0; 0]; % LED-5
+    %% Define the ideal scaled down LED pattern layout on the Target S/C    
+    led1_pos_target = [0; -d_initial; 0]; % LED-1
+    led2_pos_target = [0; 0; -d_initial]; % LED-2
+    led3_pos_target = [0; d_initial; 0]; % LED-3
+    led4_pos_target = [0; 0; d_initial]; % LED-4
+    led5_pos_target = [d_initial; 0; 0]; % LED-5
+    
+    % LED Pattern Matrix on the Target Frame
+    xnt_ideal = [led1_pos_target, led2_pos_target, led3_pos_target, led4_pos_target, led5_pos_target];
+
+    %% LED pattern on the Chaser S/C
+
+    % Position of the LED on the camera sensor
+    xy1 = d_initial*((cosd(Az+gamma)*cosd(alpha))-(sind(El+beta)*sind(Az+gamma)*sind(alpha)));
+    
+    xz1 = -d_initial*cosd(El+beta)*sind(alpha);
+    
+    xy2 = d_initial*((cosd(Az+gamma)*sind(alpha))+(sind(El+beta)*sind(Az+gamma)*cosd(alpha)));
+    
+    xz2 = d_initial*cosd(El+beta)*cosd(alpha);
+    
+    xy3 = -d_initial*((cosd(Az+gamma)*cosd(alpha))-(sind(El+beta)*sind(Az+gamma)*sind(alpha)));
+    
+    xz3 = d_initial*cosd(El+beta)*sind(alpha);
+    
+    xy4 = -d_initial*((cosd(Az+gamma)*sind(alpha))+(sind(El+beta)*sind(Az+gamma)*cosd(alpha)));
+    
+    xz4 = -d_initial*cosd(El+beta)*cosd(alpha);
+    
+    xy5 = d_initial*(cosd(El+beta)*sind(Az+gamma));
+    
+    xz5 = -d_initial*sind(El+beta);
+
+    led1_pos_chaser = [xy1; relative_state_chaser(2,:); xz1];
+
+    led2_pos_chaser = [xy2; relative_state_chaser(2,:); xz2];
+
+    led3_pos_chaser = [xy3; relative_state_chaser(2,:); xz3];
+
+    led4_pos_chaser = [xy4; relative_state_chaser(2,:); xz4];
+
+    led5_pos_chaser = [xy5; relative_state_chaser(2,:); xz5];
+
+    % LED Pattern Matrix on the Chaser Frame 
+    xnc = [led1_pos_chaser, led2_pos_chaser, led3_pos_chaser, led4_pos_chaser, led5_pos_chaser];
 
     %% Kalman Filter Initialisation
 
     dt = 1e-3;
-    
+
     A = [0 0 0 dt 0 0;
         0 1 0 0 dt 0;
         0 0 0 0 0 dt;
@@ -46,13 +86,11 @@ function [relative_state_chaser, absolute_state_chaser] = navigation_vbn(relativ
         0 0 0 0 0 0]; % State Transition Matrix
 
     B = [0 0 0;
-     0 dt^2/2 0;
-     0 0 0;
-     0 0 0;
-     0 dt^3 0;
-     0 0 0]; % Control Matrix
-
-    P = diag([1e-4, 1e-4, 1e-4, 1e-4, 1e-4, 1e-4]); % Process Covariance Matrix
+        0 dt^2/2 0;
+        0 0 0;
+        0 0 0;
+        0 dt^3 0;
+        0 0 0]; % Control Matrix
 
     Q = diag([0, 0, 0, 5e-5, 5e-5, 5e-5]); % Process Noise Covariance Matrix
 
@@ -65,22 +103,11 @@ function [relative_state_chaser, absolute_state_chaser] = navigation_vbn(relativ
 
     H = eye(6); % Measurement Noise Matrix
 
-    xk = d_initial;
-
     range = range_initial;
 
     %% Implementation of the Kalman Filter
 
-    Az = atand(xk/focal_length); % Initial Azimuth Angle
-    El = atand(xk/focal_length); % Initial Elevation Angle
-    
-    relative_pos_chaser = [(cos(El).*range).*cos(Az); (cos(El).*range).*sin(Az); range.*sin(El)];
-    
-    range = sqrt( ...
-        relative_pos_chaser(1,:).^2 + ...
-        relative_pos_chaser(2,:).^2 + ...
-        relative_pos_chaser(3,:).^2 ...
-        );
+    relative_pos_chaser = [(cosd(El).*range).*cosd(Az); (cosd(El).*range).*sind(Az); range.*sind(El)];
     
     relative_state_chaser_measured = [relative_pos_chaser; ...
     relative_state_chaser(4,:); relative_state_chaser(5,:); relative_state_chaser(6,:)];
@@ -99,19 +126,21 @@ function [relative_state_chaser, absolute_state_chaser] = navigation_vbn(relativ
     P = (eye(size(P)) - (K * H)) * P_minus;
     
     relative_state_chaser = x_hat;
-
-    % disp("Relative State")
-    % disp(relative_state_chaser)
     
     absolute_state_chaser = relative_state_chaser + absolute_state_target;
-    
-    range = sqrt( ...
-        relative_state_chaser(1,:).^2 + ...
-        relative_state_chaser(2,:).^2 + ...
-        relative_state_chaser(3,:).^2 ...
-        );
-    
-    %disp("Range")
-    %disp(range)
+
+    % Check for conditions using a loop
+    for i = 4:12-1
+        condition = relative_state_chaser(2, 1) >= station_keeping_points(i, 2) & relative_state_chaser(2, 1) <= station_keeping_points(i+1, 2);
+        if condition
+            desired_relative_state = [station_keeping_points(i+1, 1:3), 0, 0, 0];
+            mode = "Final";
+            break; % exit the loop if a condition is met
+
+        else
+            desired_relative_state = [0, 0, 0, 0, 0, 0];
+            mode = "Docked";
+        end
+    end
     
 end
