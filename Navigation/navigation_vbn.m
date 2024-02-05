86ad151e82b38241c4444d9bea7c3a82f4c6f916
deepanshu6901 <deepanshupunjabi98@gmail.com>
2024-02-05 22:46:27 +0530

    Implemented functions for determining the positions of LEDs on the spacecrafts

--- Navigation/navigation_vbn.m
+++ Navigation/navigation_vbn.m
@@ -13,11 +13,6 @@ function [relative_state_chaser, absolute_state_chaser, P, alpha, beta, ...
 
     %% Define the camera parameters
     focal_length = 0.2; % m, Focal length of the Camera
-    ymax = 0.1; % m, Maximum height of the Camera Sensor
-    zmax = 0.1; % m, Maximum width of the Camera Sensor
-
-    AzMax = atand(ymax/focal_length); % deg, Maximum Azimuth angle for the Camera Sensor in the XY plane
-    ElMax = atand(zmax/focal_length); % deg, Maximum Elevation angle for the Camera Sensor in the ZX plane
 
     D = 0.05; % m, Distance between the LEDs
 
@@ -26,53 +21,11 @@ function [relative_state_chaser, absolute_state_chaser, P, alpha, beta, ...
     Az = atand(d_initial/focal_length); % Initial Azimuth Angle
     El = atand(d_initial/focal_length); % Initial Elevation Angle
 
-    center_camera_sensor = [0; ymax/2; zmax/2]; % Position of LED-5 on the Camera Sensor for the 2 spacecrafts to be aligned
-
-    %% Define the ideal scaled down LED pattern layout on the Target S/C    
-    led1_pos_target = [0; -d_initial; 0]; % LED-1
-    led2_pos_target = [0; 0; -d_initial]; % LED-2
-    led3_pos_target = [0; d_initial; 0]; % LED-3
-    led4_pos_target = [0; 0; d_initial]; % LED-4
-    led5_pos_target = [d_initial; 0; 0]; % LED-5
-    
-    % LED Pattern Matrix on the Target Frame
-    xnt_ideal = [led1_pos_target, led2_pos_target, led3_pos_target, led4_pos_target, led5_pos_target];
-
-    %% LED pattern on the Chaser S/C
-
-    % Position of the LED on the camera sensor
-    xy1 = d_initial*((cosd(Az+gamma)*cosd(alpha))-(sind(El+beta)*sind(Az+gamma)*sind(alpha)));
-    
-    xz1 = -d_initial*cosd(El+beta)*sind(alpha);
-    
-    xy2 = d_initial*((cosd(Az+gamma)*sind(alpha))+(sind(El+beta)*sind(Az+gamma)*cosd(alpha)));
-    
-    xz2 = d_initial*cosd(El+beta)*cosd(alpha);
-    
-    xy3 = -d_initial*((cosd(Az+gamma)*cosd(alpha))-(sind(El+beta)*sind(Az+gamma)*sind(alpha)));
-    
-    xz3 = d_initial*cosd(El+beta)*sind(alpha);
-    
-    xy4 = -d_initial*((cosd(Az+gamma)*sind(alpha))+(sind(El+beta)*sind(Az+gamma)*cosd(alpha)));
-    
-    xz4 = -d_initial*cosd(El+beta)*cosd(alpha);
-    
-    xy5 = d_initial*(cosd(El+beta)*sind(Az+gamma));
-    
-    xz5 = -d_initial*sind(El+beta);
-
-    led1_pos_chaser = [xy1; relative_state_chaser(2,:); xz1];
-
-    led2_pos_chaser = [xy2; relative_state_chaser(2,:); xz2];
-
-    led3_pos_chaser = [xy3; relative_state_chaser(2,:); xz3];
-
-    led4_pos_chaser = [xy4; relative_state_chaser(2,:); xz4];
-
-    led5_pos_chaser = [xy5; relative_state_chaser(2,:); xz5];
-
-    % LED Pattern Matrix on the Chaser Frame 
-    xnc = [led1_pos_chaser, led2_pos_chaser, led3_pos_chaser, led4_pos_chaser, led5_pos_chaser];
+    %% Determining the positions of LEDs on the spacecrafts
+    % Uncomment the following lines in the section to determine the
+    % position of the LEDs on the spacecrafts
+    % xnc = led_positions_chaser(d_initial, Az, El, alpha, beta, gamma, relative_state_chaser);
+    % xnt = led_positions_target(d_initial);
 
     %% Kalman Filter Initialisation
 
